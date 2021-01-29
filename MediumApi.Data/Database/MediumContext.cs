d83c172152063fa1c80275a108967b1c687edcfc
Gulmirza <mirivhbv@gmail.com>
2021-01-29 13:28:37 +0400

    Adds database context class

---
+++ MediumApi.Data/Database/MediumContext.cs
@@ -0,0 +1,26 @@
+﻿using MediumApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediumApi.Data.Database
+{
+    public class MediumContext : DbContext
+    {
+        #region Ctors
+        public MediumContext()
+        {
+        }
+
+        public MediumContext(DbContextOptions<MediumContext> options) : base(options)
+        {
+            // TODO: seed db
+        }
+
+        #endregion
+
+        #region Properties
+
+        public DbSet<Post> Posts { get; set; }
+
+        #endregion
+    }
+}
