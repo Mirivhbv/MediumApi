3c424dd8b1f843a02d0ad749d7e9adc960745b44
Gulmirza <mirivhbv@gmail.com>
2021-01-29 13:57:32 +0400

    Implements test database

---
+++ MediumApi.Data.Test/Infrastructure/DatabaseInitializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using MediumApi.Data.Database;
+using MediumApi.Domain.Entities;
+
+namespace MediumApi.Data.Test.Infrastructure
+{
+    public class DatabaseInitializer
+    {
+        public static void Initialize(MediumContext context)
+        {
+            if (context.Posts.Any())
+                return;
+
+            Seed(context);
+        }
+
+        private static void Seed(MediumContext context)
+        {
+            var posts = new[]
+            {
+                new Post
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Among us",
+                    Description = "Once upon a time, there were famous rumor about sealing persons..."
+                },
+                new Post
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Call of duty",
+                    Description = "let me clarify one thing about future conversations."
+                }
+            };
+
+            context.Posts.AddRange(posts);
+            context.SaveChanges();
+        }
+    }
+}
---
+++ MediumApi.Data.Test/Infrastructure/TestBase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using MediumApi.Data.Database;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediumApi.Data.Test.Infrastructure
+{
+    public class TestBase : IDisposable
+    {
+        protected readonly MediumContext Context;
+
+        public TestBase()
+        {
+            var options = new DbContextOptionsBuilder<MediumContext>().UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            Context = new MediumContext(options);
+
+            Context.Database.EnsureCreated();
+
+            DatabaseInitializer.Initialize(Context);
+        }
+
+        public void Dispose()
+        {
+            Context.Database.EnsureDeleted();
+
+            Context.Dispose();
+        }
+    }
+}
