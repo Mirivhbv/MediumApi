458b5863a0b54203b3f6924bae682ecbfef77321
Gulmirza <mirivhbv@gmail.com>
2021-01-29 13:15:07 +0400

    Adds simple post entity

---
+++ MediumApi.Domain/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MediumApi.Domain.Entities
+{
+    public partial class Post
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+    }
+}
