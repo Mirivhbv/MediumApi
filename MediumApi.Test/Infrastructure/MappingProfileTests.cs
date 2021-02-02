969c324ff7b45ef2f06d7637f87f14d6ec59d199
Gulmirza <mirivhbv@gmail.com>
2021-02-02 16:31:07 +0400

    Applies api project tests

---
+++ MediumApi.Test/Infrastructure/MappingProfileTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AutoMapper;
+using FluentAssertions;
+using MediumApi.Domain.Entities;
+using MediumApi.Infrastructure.AutoMapper;
+using MediumApi.Models;
+using Xunit;
+
+namespace MediumApi.Test.Infrastructure
+{
+    public class MappingProfileTests
+    {
+        private readonly CreatePostModel _createPostModel;
+        private readonly IMapper _mapper;
+
+        public MappingProfileTests()
+        {
+            var mockMapper = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new MappingProfile());
+            });
+            _mapper = mockMapper.CreateMapper();
+
+            _createPostModel = new CreatePostModel
+            {
+                Title = "Title",
+                Description = "Description"
+            };
+        }
+
+        [Fact]
+        public void Map_post__CreatePostModel__should_have_valid_config()
+        {
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<Post, CreatePostModel>());
+            
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void Map_post_to_post__should_have_valid_config()
+        {
+            var configuration = new MapperConfiguration(cfg => cfg.CreateMap<Post, Post>());
+
+            configuration.AssertConfigurationIsValid();
+        }
+
+        [Fact]
+        public void Map__CreatePostModel__post()
+        {
+            var post = _mapper.Map<Post>(_createPostModel);
+
+            post.Title.Should().Be(_createPostModel.Title);
+            post.Description.Should().Be(_createPostModel.Description);
+        }
+    }
+}
