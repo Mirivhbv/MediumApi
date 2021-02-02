de6ab3a7d5dc613f9802f9caaa6a7d04791ba945
Gulmirza <mirivhbv@gmail.com>
2021-02-02 11:19:08 +0400

    Adds Commands for creating post

---
+++ MediumApi.Service/Command/CreatePostCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using MediumApi.Domain.Entities;
+
+namespace MediumApi.Service.Command
+{
+    public class CreatePostCommand : IRequest<Post>
+    {
+        public Post Post { get; set; }
+    }
+}
---
+++ MediumApi.Service/Command/CreatePostCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using MediumApi.Data.Repository;
+using MediumApi.Domain.Entities;
+
+namespace MediumApi.Service.Command
+{
+    public class CreatePostCommandHandler : IRequestHandler<CreatePostCommand, Post>
+    {
+        private readonly IRepository<Post> _postRepository;
+
+        public CreatePostCommandHandler(IRepository<Post> postRepository)
+        {
+            _postRepository = postRepository;
+        }
+
+        public async Task<Post> Handle(CreatePostCommand request, CancellationToken cancellationToken)
+        {
+            return await _postRepository.AddAsync(request.Post);
+        }
+    }
+}
