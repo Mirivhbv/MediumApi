ed11f63603a918eafb99d946cab622c5d82a5bdd
Gulmirza <mirivhbv@gmail.com>
2021-02-03 11:24:49 +0400

    Creates separate post repository

---
+++ MediumApi.Data/Repository/IPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediumApi.Domain.Entities;
+
+namespace MediumApi.Data.Repository
+{
+    public interface IPostRepository : IRepository<Post>
+    {
+        Task<List<Post>> GetPostsAsync(CancellationToken cancellationToken);
+    }
+}
---
+++ MediumApi.Data/Repository/PostRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediumApi.Data.Database;
+using MediumApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediumApi.Data.Repository
+{
+    public class PostRepository : Repository<Post>, IPostRepository
+    {
+        public PostRepository(MediumContext mediumContext) : base(mediumContext)
+        {
+        }
+
+        public async Task<List<Post>> GetPostsAsync(CancellationToken cancellationToken)
+        {
+            return await MediumContext.Posts.ToListAsync(cancellationToken);
+        }
+    }
+}
